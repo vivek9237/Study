85df9f61b452b7cf12edfb6c0a6de0d0ff99bfe1
Vivek Kumar Mohanty <vivek.ku.mohanty@gmail.com>
2020-11-23 11:54:45 +0530

    REMOVED SOME FILES
    REMOVED SOME FILES

--- virendra_sql.sql
+++
@@ -1,4 +0,0 @@
-select t1.name,t2.name,t2.address from table1 t1 inner join table2 t2 on(t1.name=t2.name)
-union
-select a.t1name,t3.name,t3.address from (select t1.name t1name from table1 t1 left outer join table2 t2 on(t1.name=t2.name) where t2.name is null)a
-inner join table3 t3 on (a.t1name=t3.name);
